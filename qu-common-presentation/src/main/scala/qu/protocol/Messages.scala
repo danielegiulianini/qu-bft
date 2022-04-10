4a77b3f47252e930df964a0d15fb49f23386f2a8
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-04-10 13:49:57 +0200

    add basic version of protcol messages

---
+++ qu-common-presentation/src/main/scala/qu/protocol/Messages.scala
@@ -0,0 +1,20 @@
+package qu.protocol
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+
+
+object Messages {
+
+  @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "className")
+  trait Operation[ReturnValueT, ObjectT] {
+    def compute(obj: ObjectT): ReturnValueT
+  }
+
+  trait Query[ReturnValueT, ObjectT] extends Operation[ReturnValueT, ObjectT]
+
+  trait Update[ReturnValueT, ObjectT] extends Operation[ReturnValueT, ObjectT]
+
+  final case class Response[ReturnValueT](responseCode:Int, answer: ReturnValueT, order: Int)
+
+  final case class Request[ReturnValueT, ObjectT](operation: Messages.Operation[ReturnValueT, ObjectT])
+}
---
+++ qu-common-presentation/src/main/scala/qu/protocol/ResponseCodes.scala
@@ -0,0 +1,5 @@
+package qu.protocol
+
+object ResponseCodes {
+
+}
