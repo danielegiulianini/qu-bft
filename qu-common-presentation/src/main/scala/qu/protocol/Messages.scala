8f86c5cfda96d958b9ba195c9a0c537501f20c75
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-04-10 16:52:42 +0200

    add example of funspec test

--- qu-common-presentation/src/main/scala/qu/protocol/Messages.scala
+++ qu-common-presentation/src/main/scala/qu/protocol/Messages.scala
@@ -14,7 +14,7 @@ object Messages {
 
   trait Update[ReturnValueT, ObjectT] extends Operation[ReturnValueT, ObjectT]
 
-  final case class Response[ReturnValueT](responseCode:Int, answer: ReturnValueT, order: Int)
-
   final case class Request[ReturnValueT, ObjectT](operation: Messages.Operation[ReturnValueT, ObjectT])
+
+  final case class Response[ReturnValueT](responseCode:Int, answer: ReturnValueT, order: Int)
 }
---
+++ qu-common-presentation/src/test/scala/ExampleTest.scala
@@ -0,0 +1,7 @@
+import org.scalatest.Assertion
+import org.scalatest.funspec.AnyFunSpec
+
+class ExampleTest extends AnyFunSpec
+{
+  println("ok")
+}
