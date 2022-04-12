a6a23ecc20e759fe5f3277469120eb704218b8ca
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-04-12 22:17:34 +0200

    add pluggable optimization performing caching of methodDescriptors

---
+++ qu-common-presentation/src/main/scala/qu/protocol/MemoHelper.scala
@@ -0,0 +1,7 @@
+package qu.protocol
+
+object MemoHelper {
+  def memoize[I, O](f: I => O): I => O = new collection.mutable.HashMap[I, O]() {
+    override def apply(key: I) = getOrElseUpdate(key, f(key))
+  }
+}
--- qu-common-presentation/src/main/scala/qu/protocol/MethodDescriptorFactory.scala
+++ qu-common-presentation/src/main/scala/qu/protocol/MethodDescriptorFactory.scala
@@ -3,21 +3,20 @@ package qu.protocol
 import io.grpc.MethodDescriptor
 import qu.protocol.Messages.{Request, Response}
 
-
 trait MethodDescriptorFactory {
   type Marshallable[T]
 
   //a deterministic function on parameter types...
-  def getGenericSignature[T, U]: String   //type Signaturable[T, U] <: {def getGenericSignature: String}
+  def getGenericSignature[T: Marshallable, U: Marshallable](): String //type Signaturable[T, U] <: {def getGenericSignature: String}
 
-  def marshallerFor[T](implicit clz: Marshallable[T]): MethodDescriptor.Marshaller[T]
+  def marshallerFor[T: Marshallable](): MethodDescriptor.Marshaller[T]
 
   //todo: here I require Marhallable of Response instead of only a marshaller of U and T so marshallerForResponse etc. are not needed in MDFactory
   def generateMethodDescriptor[T, U](methodName: String, serviceName: String)(implicit enc: Marshallable[Request[T, U]], enc3: Marshallable[Response[T]], enc2: Marshallable[T], dec: Marshallable[U]): MethodDescriptor[Messages.Request[T, U], Messages.Response[T]] = {
     MethodDescriptor.newBuilder(
       marshallerFor[Messages.Request[T, U]],
       marshallerFor[Messages.Response[T]])
-      .setFullMethodName(MethodDescriptor.generateFullMethodName(serviceName,        // methodName + implicitly[Signaturable[T, U]].getGenericSignature))
+      .setFullMethodName(MethodDescriptor.generateFullMethodName(serviceName, // methodName + implicitly[Signaturable[T, U]].getGenericSignature))
         methodName + getGenericSignature[T, U]))
       .setType(MethodDescriptor.MethodType.UNARY)
       .setSampledToLocalTracing(true)
@@ -25,10 +24,27 @@ trait MethodDescriptorFactory {
   }
 }
 
+//an optimization that leverages flyweight pattern to avoid regenerating
+trait CachingMethodDescriptorFactory extends MethodDescriptorFactory {
+  //self: MethodDescriptorFactory =>
+  //mi serve un identificativo della coppia di metodi
+  override def generateMethodDescriptor[T, U](methodName: String, serviceName: String)
+                                             (implicit enc: Marshallable[Request[T, U]],
+                                              enc3: Marshallable[Response[T]],
+                                              enc2: Marshallable[T], dec: Marshallable[U]):
+  MethodDescriptor[Messages.Request[T, U], Messages.Response[T]] = {
+    //super referes to the next in the chain
+   MemoHelper.memoize((_:String) => super.generateMethodDescriptor(methodName, serviceName)).apply(super.getGenericSignature[T, U])
+    ///analogous to: this.methodDescriptors.computeIfAbsent(op.getClass, (c: Class[_]) => KvGson.generateMethodDescriptor(classOf[KvGson.Wrapping[_, _]]))
+  }
+  //memo helper def not put here as inner def since it could be reused elsewhere
+}
+
+
 trait MarshallerFactory {
   type Marshallable[T]
 
-  def marshallerFor[T](implicit clz: Marshallable[T]): MethodDescriptor.Marshaller[T]
+  def marshallerFor[T:Marshallable]: MethodDescriptor.Marshaller[T]
 }
 
 
