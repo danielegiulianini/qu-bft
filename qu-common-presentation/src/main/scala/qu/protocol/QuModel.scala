82c170a115d887e947b3188ca4df450ac94bd681
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-04-13 16:43:15 +0200

    fix cyclic dependency for QuModel type aliases

--- qu-common-presentation/src/main/scala/qu/protocol/QuModel.scala
+++ qu-common-presentation/src/main/scala/qu/protocol/QuModel.scala
@@ -9,15 +9,13 @@ trait QuModel {
   type ClientId
   type ServerId
   type Time
-  type LogicalTimestamp = (Time, /*Boolean, */String, ClientId, OHS)
   type Candidate = (LogicalTimestamp, LogicalTimestamp)
-
-
-  //or:   type LogicalTimestamp <: { val time; val barrierFlag;
-
+  type LogicalTimestamp <: { val time: Int; val ohs: OHS }  //barrierFlag
+  //type LogicalTimestamp = (Time, /*Boolean, */ String, ClientId, OHS)
   type OperationType
-  type α  //authenticator
+  type α //authenticator
 
+  //number of replica histories in the object history set in which it appears
   def order(candidate: Candidate, ohs: OHS): Int
 
   def latestCandidate(ohs: OHS): Candidate
@@ -36,20 +34,28 @@ trait QuModel {
 }
 
 //what is self-independent can be put as other trait/class and plugged by mixin
-
-case class MyLogicalTimestamp(a:String)
+case class MyLogicalTimestamp(a: String)
 
 trait AbstractQuModel extends QuModel {
-  //cyclic dep: ReplicaHistory depends on LogicalTimestamp that depends on OHS that depends on ReplicaHistory
-  override type OHS = ServerId => ReplicaHistory
+  //or structural type? so naming
+  override type OHS = ServerId => (ReplicaHistory, α)  //cyclic dep: ReplicaHistory depends on LogicalTimestamp that depends on OHS that depends on ReplicaHistory
 
   override type ReplicaHistory = Set[Candidate]
 
   override type Time = Int
+
   override type ClientId = String
 
-  //since RH is a ordered set must define ordering for LogicalTimestamp
+  //now I can add it
+  //def latestTime(ohs: OHS): LogicalTimestamp =
+
+
+  //since RH is a ordered set must define ordering for LogicalTimestamp, that actually requires
 
 
   //def latestTime(rh: ReplicaHistory): LogicalTimestamp = rh.
 }
+
+
+
+
