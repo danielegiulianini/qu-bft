1f3c4486a225200170ee73ba7746a56b73902eab
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-04-13 22:10:07 +0200

    refine family polymorphism for QuModel with a more concrete layer of instanced types (OHS and LogicalTimestamp) and latestTime utility

--- qu-common-presentation/src/main/scala/qu/protocol/QuModel.scala
+++ qu-common-presentation/src/main/scala/qu/protocol/QuModel.scala
@@ -1,5 +1,7 @@
 package qu.protocol
 
+import scala.collection.SortedSet
+
 
 trait QuModel {
   type Operation
@@ -9,8 +11,9 @@ trait QuModel {
   type ClientId
   type ServerId
   type Time
+  //type Candidate = <: { val lt: LogicalTimestamp; val ltCo: LogicalTimestamp }
   type Candidate = (LogicalTimestamp, LogicalTimestamp)
-  type LogicalTimestamp <: { val time: Int; val ohs: OHS }  //barrierFlag
+  type LogicalTimestamp <: {val time: Int; val barrierFlag: Boolean; val clientId: ClientId; val ohs: OHS} //barrierFlag
   //type LogicalTimestamp = (Time, /*Boolean, */ String, ClientId, OHS)
   type OperationType
   type α //authenticator
@@ -22,8 +25,6 @@ trait QuModel {
 
   def latestTime(rh: ReplicaHistory): LogicalTimestamp
 
-  //def latestTime(ohs: OHS): LogicalTimestamp
-
   def classify(ohs: OHS): (OperationType, Candidate, Candidate) //barrierflag to add
   // return tuples or  case classes (c.c. that extends ? it depends if i want to access them...
 
@@ -33,21 +34,19 @@ trait QuModel {
 
 }
 
-//what is self-independent can be put as other trait/class and plugged by mixin
-case class MyLogicalTimestamp(a: String)
-
 trait AbstractQuModel extends QuModel {
-  //or structural type? so naming
-  override type OHS = ServerId => (ReplicaHistory, α)  //cyclic dep: ReplicaHistory depends on LogicalTimestamp that depends on OHS that depends on ReplicaHistory
-
-  override type ReplicaHistory = Set[Candidate]
 
   override type Time = Int
 
   override type ClientId = String
 
+  override type ReplicaHistory = SortedSet[Candidate]
+
+  //or structural type? so I can name
+  override type OHS = ServerId => (ReplicaHistory, α) //cyclic dep: ReplicaHistory depends on LogicalTimestamp that depends on OHS that depends on ReplicaHistory
+
   //now I can add it
-  //def latestTime(ohs: OHS): LogicalTimestamp =
+  //def latestTime(ohs: OHS): LogicalTimestamp = ohs.
 
 
   //since RH is a ordered set must define ordering for LogicalTimestamp, that actually requires
@@ -57,5 +56,29 @@ trait AbstractQuModel extends QuModel {
 }
 
 
+trait AnstractAbstractQuModel extends AbstractQuModel {
+  override type OHS = Map[ServerId, (ReplicaHistory, α)] //cyclic dep: ReplicaHistory depends on LogicalTimestamp that depends on OHS that depends on ReplicaHistory
 
+  implicit val OHSOrdering: Ordering[OHS] = (x: OHS, y: OHS) => x.toString compare y.toString
+
+  override type LogicalTimestamp = MyLogicalTimestamp
+
+  def latestTime(ohs: OHS): LogicalTimestamp = {
+    ohs
+      .values
+      .map(_._1)
+      .map(latestTime(_))
+      .max
+  }
+
+  //what is self-independent can be put as other trait/class and plugged by mixin
+  case class MyLogicalTimestamp(a: String, val barrierFlag: Boolean, val clientId: ClientId, val ohs: OHS) extends Ordered[MyLogicalTimestamp] {
+    //a compare method here or a (implicit) ordering in companion object
+    override def compare(that: MyLogicalTimestamp): Int = ???
+  }
+
+  def latestTime(rh: ReplicaHistory): LogicalTimestamp = rh
+    .flatMap(x => Set(x._1, x._2)) //or can use ordered/ing of candid here
+    .max
+}
 
