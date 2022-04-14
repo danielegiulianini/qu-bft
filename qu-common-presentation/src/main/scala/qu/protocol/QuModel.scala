39f86474fd7176b2fb6aa6f0043cbb31793f2427
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-04-14 21:03:20 +0200

    switch to generic modelling in QuModel

--- qu-common-presentation/src/main/scala/qu/protocol/QuModel.scala
+++ qu-common-presentation/src/main/scala/qu/protocol/QuModel.scala
@@ -4,33 +4,33 @@ import scala.collection.SortedSet
 
 
 trait QuModel {
-  type Operation
-  type ReplicaHistory
-  type HistorySet
-  type OHS
+  type Operation[T, U]
+  type ReplicaHistory[U]
+  type OHS[U]
   type ClientId
   type ServerId
   type Time
   //type Candidate = <: { val lt: LogicalTimestamp; val ltCo: LogicalTimestamp }
-  type Candidate = (LogicalTimestamp, LogicalTimestamp)
-  type LogicalTimestamp <: {val time: Int; val barrierFlag: Boolean; val clientId: ClientId; val ohs: OHS} //barrierFlag
-  //type LogicalTimestamp = (Time, /*Boolean, */ String, ClientId, OHS)
+  type Candidate[U] = (LogicalTimestamp[_, U], LogicalTimestamp[_, U])
+  type LogicalTimestamp[T, U] <: {val time: Int; val barrierFlag: Boolean; val clientId: ClientId; val operation: Operation[T, U]; val ohs: OHS[U]} // this causes cyc dep: type LogicalTimestamp = (Time, Boolean, String, ClientId, OHS)
+
   type OperationType
+
   type α //authenticator
 
   //number of replica histories in the object history set in which it appears
-  def order(candidate: Candidate, ohs: OHS): Int
+  def order[U](candidate: Candidate[U], ohs: OHS[U]): Int
 
-  def latestCandidate(ohs: OHS): Candidate
+  def latestCandidate[U](ohs: OHS[U]): Candidate[U]
 
-  def latestTime(rh: ReplicaHistory): LogicalTimestamp
+  def latestTime[U](rh: ReplicaHistory[U]): LogicalTimestamp[_, U]
 
-  def classify(ohs: OHS): (OperationType, Candidate, Candidate) //barrierflag to add
   // return tuples or  case classes (c.c. that extends ? it depends if i want to access them...
+  def classify[U](ohs: OHS[U]): (OperationType, Candidate[U], Candidate[U]) //barrierflag to add
 
-  def compare(logicalTimestamp1: LogicalTimestamp, logicalTimestamp2: LogicalTimestamp): Int
+  def compare[U](logicalTimestamp1: LogicalTimestamp[_, U], logicalTimestamp2: LogicalTimestamp[_, U]): Int
 
-  def max(logicalTimestamp: LogicalTimestamp): Candidate
+  def max[U](logicalTimestamp: LogicalTimestamp[_, U]): Candidate[U]
 
 }
 
@@ -40,51 +40,44 @@ trait AbstractQuModel extends QuModel {
 
   override type ClientId = String
 
-  override type ReplicaHistory = SortedSet[Candidate]
+  override type ReplicaHistory[U] = SortedSet[Candidate[U]]
 
   //or structural type? so I can name
   //override type OHS = ServerId => (ReplicaHistory, α)
-
   //since RH is a ordered set must define ordering for LogicalTimestamp, that actually requires
 }
 
 
 trait AnstractAbstractQuModel extends AbstractQuModel {
   //the ones of the following that are self-independent can be put in separate trait/class and plugged by mixin
-  override type OHS = Map[ServerId, (ReplicaHistory, α)]
+  override type OHS[U] = Map[ServerId, (ReplicaHistory[U], α)]
+  override type Operation[T, U] = Messages.Operation[T, U]
 
   //or as Ordering:   implicit val MyLogicalTimestampOrdering: Ordering[MyLogicalTimestamp] = (x: MyLogicalTimestamp, y: MyLogicalTimestamp) => x.toString compare y.toString
-  override type LogicalTimestamp = MyLogicalTimestamp
+  override type LogicalTimestamp[T, U] = MyLogicalTimestamp[T, U]
 
   //clean but not considers if rh are not ordered by server...
   //implicit val OHSOrdering: Ordering[OHS] = (x: OHS, y: OHS) => x.values.toString compare y.toString
-  implicit val OHSOrdering: Ordering[OHS] = (x: OHS, y: OHS) =>
-    x.values.toString compare y.toString
+  implicit def OHSOrdering[U]: Ordering[OHS[U]] = (x: OHS[U], y: OHS[U]) => x.values.toString compare y.toString
 
-  case class MyLogicalTimestamp(time: Int, barrierFlag: Boolean, clientId: ClientId, ohs: OHS) /*extends Ordered[MyLogicalTimestamp] {
-    //a compare method here or a (implicit) ordering in companion object
-    override def compare(that: MyLogicalTimestamp): Int =
-      (self.last compare that.last) match {
-        case 0 =>
-          (self.first compare that.first) match {
-            case 0 => self.middle compare that.middle
-            case c => c
-          }
-        case c => c
-      }
-  }*/
-  implicit val MyLogicalTimestampOrdering: Ordering[MyLogicalTimestamp] =
+  case class MyLogicalTimestamp[T, U](time: Int,
+                                      barrierFlag: Boolean,
+                                      clientId: ClientId,
+                                      operation: Messages.Operation[T, U],
+                                      ohs: OHS[U])
+
+  implicit def MyLogicalTimestampOrdering[U]: Ordering[MyLogicalTimestamp[_, U]] =
     Ordering.by(lt => (lt.time, lt.barrierFlag, lt.clientId, lt.ohs))
 
-  def latestTime(ohs: OHS): LogicalTimestamp = {
+  def latestTime[U](ohs: OHS[U]): LogicalTimestamp[_, U] = {
     ohs
       .values
       .map(_._1)
-      .map(latestTime)
+      .map(latestTime[U])
       .max
   }
 
-  def latestTime(rh: ReplicaHistory): LogicalTimestamp = rh
+  def latestTime[U](rh: ReplicaHistory[U]): LogicalTimestamp[_, U] = rh
     .flatMap(x => Set(x._1, x._2))
     .max
 }
