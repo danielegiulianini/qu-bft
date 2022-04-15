eadfb5d876a52a2911140a36ad9a321793e46138
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-04-15 11:40:14 +0200

    add JacksonClientStub

--- qu-client/src/main/scala/GrpcClientStub.scala
+++ qu-client/src/main/scala/GrpcClientStub.scala
@@ -3,30 +3,35 @@ import com.google.common.util.concurrent.ListenableFuture
 import io.grpc.stub.ClientCalls
 import io.grpc.{CallOptions, ManagedChannel, MethodDescriptor}
 import qu.protocol.Messages.{Request, Response}
-import qu.protocol.{Messages, MethodDescriptorFactory}
+import qu.protocol.{JacksonMethodDescriptorFactory, MarshallerFactory, Messages, MethodDescriptorFactory}
 
 
-//this is a grpc ClientStub
-abstract class GrpcClientStub[T](var chan: ManagedChannel) { self:MethodDescriptorFactory =>
+abstract class GrpcClientStub[T](var chan: ManagedChannel) {
+  self: MethodDescriptorFactory with MarshallerFactory =>
 
   //decide where to inject
   val methodName = "todo"
   val serviceName = "todo"
 
-  def generateMethodDescriptor[U](methodName: String, serviceName: String)
-                                    (implicit enc: Marshallable[Request[T, U]],
-                                     enc3: Marshallable[Response[T]],
-                                     enc2: Marshallable[T], dec: Marshallable[U]):
-  MethodDescriptor[Messages.Request[T, U], Messages.Response[T]]
-
   def send[U](op: Messages.Request[T, U])
-                (implicit enc: Marshallable[T], dec: Marshallable[U],
-                 marshallable: Marshallable[Request[T, U]], marshallable3: Marshallable[Response[T]]):
-    ListenableFuture[Messages.Response[T]] = {
-    //todo flyWeight pattern to be used here
-    val md2 = generateMethodDescriptor[T, U](methodName, serviceName) //this.methodDescriptors.computeIfAbsent(op.getClass, (c: Class[_]) => KvGson.generateMethodDescriptor(classOf[KvGson.Wrapping[_, _]]))
+             (implicit enc: Marshallable[T], dec: Marshallable[U],
+              marshallable: Marshallable[Request[T, U]],
+              marshallable3: Marshallable[Response[T]]):
+  ListenableFuture[Messages.Response[T]] = {
+    val md2 = generateMethodDescriptor[T, U](methodName, serviceName)
     ClientCalls.futureUnaryCall(chan.newCall(md2, CallOptions.DEFAULT), op)
   }
 }
 
-//example of use: class JacksonCLientStub extends ClientStub with JacksonMethodDescriptorFactory
+class JacksonClientStub(channel: ManagedChannel) extends GrpcClientStub(channel) with JacksonMethodDescriptorFactory
+
+
+//example of use:
+//class JacksonCLientStub extends GrpcClientStub(null) with JacksonMethodDescriptorFactory
+
+//with cake pattern:
+//class JacksonCLientStub extends GrpcClientStub(null) with JacksonMethodDescriptorFactory {
+//val a = new A (declared in GrpcClientStub); val b= new B (declared in GrpcClientStub))
+//}
+//only diff with normal mixin with self-type is: every mixin doesn't call methods directly
+//on the self type but on its val
