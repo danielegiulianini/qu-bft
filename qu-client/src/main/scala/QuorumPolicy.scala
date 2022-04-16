4270872cd987537f97870bd274ad6de6159cbbee
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-04-16 15:06:04 +0200

    make QuorumPolicy generic

--- qu-client/src/main/scala/QuorumPolicy.scala
+++ qu-client/src/main/scala/QuorumPolicy.scala
@@ -4,8 +4,8 @@ import qu.protocol.Messages.Response
 
 import scala.collection.SortedSet
 
-trait QuorumPolicy {
-  def quorum[T, U](servers: Servers): (Response[T], Int, OHS[U])
+trait QuorumPolicy[U] {
+  def quorum[T](servers: Servers): (Response[T], Int, OHS[U])
 }
 
 //there are "quorumPolicy"s that actually require obj pref quorum is defined
@@ -14,7 +14,8 @@ trait QuorumPolicy {
 trait WithPreferredQuorum[A]
 class QuorumPolicy2[T:WithPreferredQuorum] extends QuorumPolicy {
   override protected[this] def quorum[T, U](servers: Servers):
-  (Response[T], Int, Map[ConcreteQuModel.ServerId, (SortedSet[(ConcreteQuModel.MyLogicalTimestamp[_, U], ConcreteQuModel.MyLogicalTimestamp[_, U])], ConcreteQuModel.α)]) = ???
+  (Response[T], Int, Map[ConcreteQuModel.ServerId, (SortedSet[(ConcreteQuModel.MyLogicalTimestamp[_, U], ConcreteQuModel.MyLogicalTimestamp[_, U])], ConcreteQuModel.α)]) =
+    ???
 }
 
 
