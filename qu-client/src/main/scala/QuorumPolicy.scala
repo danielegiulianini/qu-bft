799a8f13fd9a295ba6ed153a84a596b908ee9d8c
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-04-16 16:51:43 +0200

    fix signature of QuorumPolicy

--- qu-client/src/main/scala/QuorumPolicy.scala
+++ qu-client/src/main/scala/QuorumPolicy.scala
@@ -3,19 +3,20 @@ import qu.protocol.ConcreteQuModel.OHS
 import qu.protocol.Messages.Response
 
 import scala.collection.SortedSet
+import scala.concurrent.Future
 
 trait QuorumPolicy[U] {
-  def quorum[T](servers: Servers): (Response[T], Int, OHS[U])
+  def quorum[T](servers: Servers, ohs:OHS[U]): Future[(Response[T], Int, OHS[U])]
 }
 
 //there are "quorumPolicy"s that actually require obj pref quorum is defined
 //or better: all requires a pref quorum, but if they ignore it, sit can be
 //made of by all the servers
-trait WithPreferredQuorum[A]
-class QuorumPolicy2[T:WithPreferredQuorum] extends QuorumPolicy {
-  override protected[this] def quorum[T, U](servers: Servers):
+/*trait WithPreferredQuorum[A]
+class QuorumPolicy2[T:WithPreferredQuorum, U] extends QuorumPolicy[U] {
+  override protected def quorum[T](servers: Servers):
   (Response[T], Int, Map[ConcreteQuModel.ServerId, (SortedSet[(ConcreteQuModel.MyLogicalTimestamp[_, U], ConcreteQuModel.MyLogicalTimestamp[_, U])], ConcreteQuModel.α)]) =
     ???
-}
+}*/
 
 
