f1b5ec618cfe125a876ff7f76573df0c192150a5
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-04-18 16:05:39 +0200

    add AuthenticatedReplicaHistory obj to as a field of server's Response case class

--- qu-common-presentation/src/main/scala/qu/protocol/Messages.scala
+++ qu-common-presentation/src/main/scala/qu/protocol/Messages.scala
@@ -1,6 +1,6 @@
 package qu.protocol
 
-
+//operation modeled via Gof's command pattern
 object Messages {
 
   trait Operation[ReturnValueT, ObjectT] {
@@ -11,8 +11,8 @@ object Messages {
 
   trait Update[ReturnValueT, ObjectT] extends Operation[ReturnValueT, ObjectT]
 
-  final case class Request[ReturnValueT, ObjectT](operation: Messages.Operation[ReturnValueT, ObjectT])
+  final case class Request[ReturnValueT, ObjectT](operation: Operation[ReturnValueT, ObjectT])
 
-  final case class Response[ReturnValueT](responseCode:Int, answer: ReturnValueT, order: Int)
+  final case class Response[ReturnValueT, ObjectT](responseCode:Int, answer: ReturnValueT, order: Int, authenticatedRh: ConcreteQuModel.AuthenticatedReplicaHistory[ObjectT])
 
 }
