296bcc6c0cc95110e49bf4e8d005cbd368dee1b1
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-04-22 16:40:07 +0200

    remove unnecessary apply from MethodDescriptorFactory

--- qu-common-presentation/src/main/scala/qu/protocol/MethodDescriptorFactory.scala
+++ qu-common-presentation/src/main/scala/qu/protocol/MethodDescriptorFactory.scala
@@ -3,19 +3,16 @@ package qu.protocol
 import io.grpc.MethodDescriptor
 import qu.protocol.Messages.{Request, Response}
 
-trait MethodDescriptorFactory {
-  type Marshallable[T]
 
+trait MethodDescriptorFactory { self:MarshallerFactory =>
   //a deterministic function on parameter types...
   def getGenericSignature[T: Marshallable, U: Marshallable]: String //type Signaturable[T, U] <: {def getGenericSignature: String}
 
-  def marshallerFor[T: Marshallable](): MethodDescriptor.Marshaller[T]
-
   //todo: here I require Marhallable of Response instead of only a marshaller of U and T so marshallerForResponse etc. are not needed in MDFactory
-  def generateMethodDescriptor[T, U](methodName: String, serviceName: String)(implicit enc: Marshallable[Request[T, U]], enc3: Marshallable[Response[T]], enc2: Marshallable[T], dec: Marshallable[U]): MethodDescriptor[Messages.Request[T, U], Messages.Response[T]] = {
+  def generateMethodDescriptor[T, U](methodName: String, serviceName: String)(implicit enc: Marshallable[Request[T, U]], enc3: Marshallable[Response[T, U]], enc2: Marshallable[T], dec: Marshallable[U]): MethodDescriptor[Messages.Request[T, U], Messages.Response[T,U]] = {
     MethodDescriptor.newBuilder(
       marshallerFor[Messages.Request[T, U]],
-      marshallerFor[Messages.Response[T]])
+      marshallerFor[Messages.Response[T,U]])
       .setFullMethodName(MethodDescriptor.generateFullMethodName(serviceName, // methodName + implicitly[Signaturable[T, U]].getGenericSignature))
         methodName + getGenericSignature[T, U]))
       .setType(MethodDescriptor.MethodType.UNARY)
@@ -25,25 +22,20 @@ trait MethodDescriptorFactory {
 }
 
 //an optimization that leverages flyweight pattern to avoid regenerating method descriptors
-trait CachingMethodDescriptorFactory extends MethodDescriptorFactory {
-
+trait CachingMethodDescriptorFactory extends MethodDescriptorFactory with MarshallerFactory {
   //i need an identifier of the pair of methods
-  override abstract def generateMethodDescriptor[T, U](methodName: String, serviceName: String)
+  //override abstract is required here?
+  override def generateMethodDescriptor[T, U](methodName: String, serviceName: String)
                                                       (implicit enc: Marshallable[Request[T, U]],
-                                                       enc3: Marshallable[Response[T]],
+                                                       enc3: Marshallable[Response[T,U]],
                                                        enc2: Marshallable[T], dec: Marshallable[U]):
-  MethodDescriptor[Messages.Request[T, U], Messages.Response[T]] = {
-    //super referes to the next in the chain
+  MethodDescriptor[Messages.Request[T, U], Messages.Response[T,U]] = {
+    //super refers to the next in the chain
     MemoHelper.memoize((_: String) =>
-      super.generateMethodDescriptor(methodName, serviceName)).apply(this.getGenericSignature[T, U])
+      super.generateMethodDescriptor(methodName, serviceName))(getGenericSignature[T, U])
   }
 }
 
-
-trait MarshallerFactory {
-  type Marshallable[T]
-
-  def marshallerFor[T: Marshallable]: MethodDescriptor.Marshaller[T]
-}
-
-
+//type Marshallable[T]
+//override type Marshallable[T] = Marshallable[T]
+//def marshallerFor[T: Marshallable](): MethodDescriptor.Marshaller[T]
