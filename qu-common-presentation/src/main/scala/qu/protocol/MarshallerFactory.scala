04f987cc8676d7dd61f9179d5d943be9ca76ff9a
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-04-27 13:30:49 +0200

    convert MarshallerFactory's Marshaller from abstract type to type param

--- qu-common-presentation/src/main/scala/qu/protocol/MarshallerFactory.scala
+++ qu-common-presentation/src/main/scala/qu/protocol/MarshallerFactory.scala
@@ -6,27 +6,26 @@ import com.fasterxml.jackson.databind.json.JsonMapper
 import com.fasterxml.jackson.module.scala.{ClassTagExtensions, DefaultScalaModule, JavaTypeable}
 import io.grpc.MethodDescriptor
 
+//import that declares specific dependency
+import qu.protocol.ConcreteQuModel._
+
 import java.io.{ByteArrayInputStream, InputStream}
 
-trait MarshallerFactory {
-  type Marshallable[T]
-
+trait MarshallerFactory[Marshallable[_]] {
   def marshallerFor[T: Marshallable]: MethodDescriptor.Marshaller[T]
 }
 
 
 //could inherit from MethodDescriptorFactory
-trait JacksonMarshallerFactory extends MarshallerFactory {  //self: MethodDescriptorFactory =>
-
-  override type Marshallable[T] = JavaTypeable[T]
-
+trait JacksonMarshallerFactory extends MarshallerFactory[JavaTypeable] {  //self: MethodDescriptorFactory =>
+  //override type Marshallable[T] = JavaTypeable[T]
   @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "className")
   class JacksonOperationMixin
 
   private val mapper = JsonMapper.builder()
     .addModule(DefaultScalaModule)
     //this allows to plug jackson features to original class (technology-agnostic) class w/o editing it
-    .addMixIn(classOf[Messages.Operation[_, _]], classOf[JacksonOperationMixin]) //.registerSubtypes(classOf[Messages.AInterface[_, _]], classOf[Messages.C1])    //not needed
+    .addMixIn(classOf[Operation[_, _]], classOf[JacksonOperationMixin]) //.registerSubtypes(classOf[Messages.AInterface[_, _]], classOf[Messages.C1])    //not needed
     .build() :: ClassTagExtensions
 
   override def marshallerFor[T: JavaTypeable]: MethodDescriptor.Marshaller[T] =
@@ -34,10 +33,8 @@ trait JacksonMarshallerFactory extends MarshallerFactory {  //self: MethodDescri
       override def stream(value: T): InputStream = {
         new ByteArrayInputStream(mapper.writeValueAsBytes(value))
       }
-
-      override def parse(stream: InputStream): T = {
+      override def parse(stream: InputStream): T =
         mapper.readValue[T](stream)
-      }
     }
 
 }
