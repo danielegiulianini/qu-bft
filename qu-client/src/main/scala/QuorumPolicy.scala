9195b624345a0974e5e9b21008749915fdd4f240
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-05-02 11:28:10 +0200

    implement backoff for client side

--- qu-client/src/main/scala/QuorumPolicy.scala
+++ qu-client/src/main/scala/QuorumPolicy.scala
@@ -1,6 +1,7 @@
 import monix.execution.Scheduler
 import monix.execution.schedulers.SchedulerService
-import scala.concurrent.duration.DurationInt
+
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
 import scala.concurrent.{Future, Promise}
 import scala.util.{Failure, Success}
 
@@ -12,39 +13,32 @@ trait QuorumPolicy[U] {
   def quorum[T](operation: Operation[T, U], ohs: OHS[U]): Future[(T, Int, OHS[U])]
 }
 
-trait SimpleBroadcastPolicy[U, MarshallableA[_]] extends QuorumPolicy[U] {
+trait SimpleBroadcastPolicy[U, Marshallable[_]] extends QuorumPolicy[U] {
   implicit val scheduler: SchedulerService = Scheduler.singleThread("") //concurrency level configurable by user??
 
   //is it possible to have overlapping calls to schedule (only so it's convenient to use >1 threads)?? no, actually!
-
-  //overridinabstract tye istead of passing type param through
-  trait A extends GrpcClientStub[U] {
-    override type Marshallable[A] = MarshallableA[A]
-  }
-
-  type AType = A
-  private val servers: Map[ServerId, AType] = Map()
+  protected val servers: Map[ServerId, GrpcClientStub[U, Marshallable]]
 
   //protected for not exposing quorum def to client (for emulating costructor with private field)...it's not allowed otherwise!
   override def quorum[T](operation: Operation[T, U], ohs: OHS[U]):
   Future[(T, Int, OHS[U])] = {
     //private nested method
-    def quorumImpl2[T](operation: Operation[T, U], ohs: OHS[U],
+    def quorumImpl2(operation: Operation[T, U], ohs: OHS[U],
                        promise: Promise[(Set[Response[T, U]], OHS[U])],
-                       successSet: Map[ServerId, Response[T, U]])(implicit marshallable: MarshallableA[T],
-                                                                  marshallable2: MarshallableA[U],
-                                                                  marshallable3a: MarshallableA[Request[T, U]],
-                                                                  marshallable4: MarshallableA[Response[T, U]]):
+                       successSet: Map[ServerId, Response[T, U]])(implicit marshallable: Marshallable[T],
+                                                                  marshallable2: Marshallable[U],
+                                                                  marshallable3a: Marshallable[Request[T, U]],
+                                                                  marshallable4: Marshallable[Response[T, U]]):
     Unit = {
       //var currentSuccessSet = successSet;
       //qui devo recuperare l'id del server e aggiornare la ohs attribuendo a lui...
       var myOhs: OHS[U] = ohs
-      var currentSuccessSet = successSet;
+      var currentSuccessSet = successSet
       val cancelable = scheduler.scheduleOnce(3.seconds)(quorumImpl2(operation, myOhs, promise, successSet)) //passing all the servers  the first time
       (servers -- successSet.keySet)
         .map(kv => (kv._1,
-          //PUNTO DOVE ESPONGO IMPLEMENTAZIONE!
-          kv._2.send[T](operation = Request[T, U](operation, ohs)))) //why this (more readable) nt working??:  .map((k: ServerId, s: JacksonClientStub[U]) => (k, s.send[T](operation = Request(operation), callOptions = CallOptions.DEFAULT)(enc = a, implicitly, implicitly,implicitly)))
+          //PUNTO DOVE ESPONGO IMPLEMENTAZIONE (Request)!
+          kv._2.send[T](operation = Request[T, U](operation, ohs)))) //why this (more readable) nt working?? need case?:  .map((k: ServerId, s: JacksonClientStub[U]) => (k, s.send[T](operation = Request(operation), callOptions = CallOptions.DEFAULT)(enc = a, implicitly, implicitly,implicitly)))
         .foreach(kv => kv._2.onComplete({
           case Success(response) if response.responseCode == StatusCode.SUCCESS =>
             //here a mutex...
@@ -58,6 +52,7 @@ trait SimpleBroadcastPolicy[U, MarshallableA[_]] extends QuorumPolicy[U] {
         }))
     }
 
+
     val promise = Promise[(Set[Response[T, U]], OHS[U])]()
     for {
       (successSet, ohs) <- promise.future
@@ -65,4 +60,14 @@ trait SimpleBroadcastPolicy[U, MarshallableA[_]] extends QuorumPolicy[U] {
     } yield (2, 2, ohs)
     null
   }
+
+  //to move in scheduler class
+  private def scheduleOnceWithPromise(duration: FiniteDuration): Future[Void] = {
+    val promise = Promise()
+    scheduler.scheduleOnce (3.seconds) (())
+    promise.future
+  }
+
+  //doing nothing
+  private def backOff() = scheduleOnceWithPromise(3.seconds)
 }
