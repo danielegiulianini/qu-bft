31de62650680a8cdee5fe6b594c0f7a97f95dbef
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-05-04 22:14:27 +0200

    leverage IOption in heterogeneous container

--- qu-service/src/main/scala/HeterogeneousContainer.scala
+++ qu-service/src/main/scala/HeterogeneousContainer.scala
@@ -2,6 +2,12 @@ import scala.reflect.runtime.universe._
 
 object HeterogeneousContainer { // Typesafe heterogeneous container pattern - client
   def main(args: Array[String]): Unit = {
+
+    val a = new HeterogeneousContainer
+    a.putFavorite[Int](2)
+    a.putFavorite[String]("ciao")
+    val c = a.getFavorite[Int]
+    println("c " + c)
     /*val f: Favorites = new Favorites
     f.putFavorite(classOf[String], "Java")
     f.putFavorite(classOf[Integer], 0xcafebabe)
@@ -16,14 +22,19 @@ object HeterogeneousContainer { // Typesafe heterogeneous container pattern - cl
 
 
 class HeterogeneousContainer {
-  private val favorites: Map[TypeTag[_], Object] = Map()
+  private var favorites: Map[TypeTag[_], Any] = Map()
 
-  def putFavorite[T](`type`: TypeTag[T], instance: T): Unit = {
+  def putFavorite[T](instance: T)(implicit `type`: TypeTag[T]): Unit = {
     if (`type` == null) throw new NullPointerException("Type is null")
-    favorites + (`type`-> instance)
+    favorites = favorites + (`type` -> instance)
   }
 
-  def getFavorite[T](`type`: TypeTag[T]): T = favorites.get(`type`).asInstanceOf[T]
+  //se c'è ritorna quello che c'è tipato, altrimenti deve ritornare un option
+  def getFavorite[T](implicit `type`: TypeTag[T]): Option[T] = {
+    println("la map is: " + favorites)
+    println("cio refeprito is:" + favorites.get(`type`).get)
+    favorites.get(`type`).map(a => a.asInstanceOf[T])
+  }
 
   // def cast[A](a: Any, tag: TypeTag[A]): A = a.asInstanceOf[A]
   //`type`.cast(favorites.get(`type`))
