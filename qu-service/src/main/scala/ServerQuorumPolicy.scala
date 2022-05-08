a02b727a7867c2c7243c974804446cf0aa03ca9f
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-05-08 16:51:50 +0200

    fix ServerQuorumPolicy

--- qu-service/src/main/scala/ServerQuorumPolicy.scala
+++ qu-service/src/main/scala/ServerQuorumPolicy.scala
@@ -1,10 +1,10 @@
 import scala.concurrent.Future
 
 trait ServerQuorumPolicy[U] {
-  def objectSync[T](): Future[(T, U)]
+  def objectSync[T]():Future[Option[(T, U)]]
 }
 
 class SimpleServerQuorumPolicy[U, Marshallable[_]](stubs: Map[String, GrpcClientStub[Marshallable]])
   extends ServerQuorumPolicy[U] {
-  override def objectSync[T](): Future[(T, U)] = null
+  override def objectSync[T](): Future[Option[(T, U)]] = null
 }
