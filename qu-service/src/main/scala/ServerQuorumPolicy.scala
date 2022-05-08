54d2c0366c0d0bf03e9d698d01c5f362fc491355
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-05-08 14:41:20 +0200

    add skeleton for abstraction of ServerQuorumPolicy

---
+++ qu-service/src/main/scala/ServerQuorumPolicy.scala
@@ -0,0 +1,10 @@
+import scala.concurrent.Future
+
+trait ServerQuorumPolicy[U] {
+  def objectSync[T](): Future[(T, U)]
+}
+
+class SimpleServerQuorumPolicy[U, Marshallable[_]](stubs: Map[String, GrpcClientStub[Marshallable]])
+  extends ServerQuorumPolicy[U] {
+  override def objectSync[T](): Future[(T, U)] = null
+}
