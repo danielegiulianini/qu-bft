a37e257fc6e7aacd1fc74a0d55ac7161cf06c891
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-05-10 11:26:00 +0200

    add Marshallable implicit requirement to QuClient

--- qu-client/src/main/scala/QuClient.scala
+++ qu-client/src/main/scala/QuClient.scala
@@ -1,6 +1,13 @@
 import scala.concurrent.Future
 
+//import that declares specific dependency
+import qu.protocol.ConcreteQuModel._
+
 //most abstract possible (not bound to grpc)
-trait QuClient[U] {
-  def submit[T](op: qu.protocol.Messages.Operation[T, U]): Future[T]
+trait QuClient[U, Marshallable[_]] {
+  def submit[T](op: Operation[T, U])(implicit
+                                     marshallableRequest: Marshallable[Request[T, U]],
+                                     marshallableResponse: Marshallable[Response[Option[T]]],
+                                     marshallableRequestObj: Marshallable[Request[Object, U]],
+                                     marshallableResponseObj: Marshallable[Response[Option[Object]]]): Future[T]
 }
