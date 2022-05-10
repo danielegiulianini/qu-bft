0b28d4790e0c3846d9a95d69d8de2fd6ccd28823
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-05-10 11:41:11 +0200

    implement basic client quorum logic

--- qu-client/src/main/scala/QuorumPolicy.scala
+++ qu-client/src/main/scala/QuorumPolicy.scala
@@ -1,6 +1,4 @@
-import monix.execution.Scheduler
-import monix.execution.schedulers.SchedulerService
-
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.{DurationInt, FiniteDuration}
 import scala.concurrent.{Future, Promise}
 import scala.util.{Failure, Success}
@@ -9,65 +7,76 @@ import scala.util.{Failure, Success}
 import qu.protocol.ConcreteQuModel._
 
 
-trait QuorumPolicy[U] {
-  def quorum[T](operation: Operation[T, U], ohs: OHS[U]): Future[(T, Int, OHS[U])]
+trait QuorumPolicy[U, Marshallable[_]] {
+  def quorum[T](operation: Option[Operation[T, U]],
+                ohs: OHS)
+               (implicit
+                marshallableRequest: Marshallable[Request[T, U]],
+                marshallableResponse: Marshallable[Response[Option[T]]]): Future[(Option[T], Int, OHS)]
 }
 
-trait SimpleBroadcastPolicy[U, Marshallable[_]] extends QuorumPolicy[U] {
-  implicit val scheduler: SchedulerService = Scheduler.singleThread("") //concurrency level configurable by user??
 
-  //is it possible to have overlapping calls to schedule (only so it's convenient to use >1 threads)?? no, actually!
-  protected val servers: Map[ServerId, GrpcClientStub[U, Marshallable]]
+//basic policy
+trait SimpleBroadcastPolicy[U, Marshallable[_]] extends QuorumPolicy[U, Marshallable] {
 
-  //protected for not exposing quorum def to client (for emulating costructor with private field)...it's not allowed otherwise!
-  override def quorum[T](operation: Operation[T, U], ohs: OHS[U]):
-  Future[(T, Int, OHS[U])] = {
+  //vakues to inject
+  val q = 2
+  val r = 3
+
+  //concurrency level configurable by user??
+  //is it possible to have overlapping calls to schedule
+  // (only so it's convenient to use >1 threads)?? no, actually!
+  //dependencies shared with QuClient
+  val scheduler = new OneShotAsyncScheduler(2)
+  protected val servers: Map[ServerId, GrpcClientStub[Marshallable]]
+
+  override def quorum[T](operation: Option[Operation[T, U]],
+                         ohs: OHS)
+                        (implicit
+                         marshallableRequest: Marshallable[Request[T, U]],
+                         marshallableResponse: Marshallable[Response[Option[T]]]): Future[(Option[T], Int, OHS)] = {
     //private nested method
-    def quorumImpl2(operation: Operation[T, U], ohs: OHS[U],
-                       promise: Promise[(Set[Response[T, U]], OHS[U])],
-                       successSet: Map[ServerId, Response[T, U]])(implicit marshallable: Marshallable[T],
-                                                                  marshallable2: Marshallable[U],
-                                                                  marshallable3a: Marshallable[Request[T, U]],
-                                                                  marshallable4: Marshallable[Response[T, U]]):
-    Unit = {
-      //var currentSuccessSet = successSet;
-      //qui devo recuperare l'id del server e aggiornare la ohs attribuendo a lui...
-      var myOhs: OHS[U] = ohs
+    def quorumImpl2(completionPromise: Promise[(Set[Response[Option[T]]], OHS)],
+                    successSet: Map[ServerId, Response[Option[T]]])
+                   (implicit
+                    marshallableRequest: Marshallable[Request[T, U]],
+                    marshallableResponse: Marshallable[Response[Option[T]]]): Future[(Set[Response[Option[T]]], OHS)] = {
+      var myOhs: OHS = ohs
       var currentSuccessSet = successSet
-      val cancelable = scheduler.scheduleOnce(3.seconds)(quorumImpl2(operation, myOhs, promise, successSet)) //passing all the servers  the first time
+      val cancelable = scheduler.scheduleOnceAsCallback(3.seconds)(quorumImpl2(completionPromise, successSet)) //passing all the servers  the first time
+
       (servers -- successSet.keySet)
         .map(kv => (kv._1,
-          //PUNTO DOVE ESPONGO IMPLEMENTAZIONE (Request)!
-          kv._2.send[T](operation = Request[T, U](operation, ohs)))) //why this (more readable) nt working?? need case?:  .map((k: ServerId, s: JacksonClientStub[U]) => (k, s.send[T](operation = Request(operation), callOptions = CallOptions.DEFAULT)(enc = a, implicitly, implicitly,implicitly)))
+          kv._2.send2[Request[T, U], Response[Option[T]]](toBeSent = Request(operation, ohs))))
         .foreach(kv => kv._2.onComplete({
           case Success(response) if response.responseCode == StatusCode.SUCCESS =>
-            //here a mutex...
-            currentSuccessSet = currentSuccessSet + ((kv._1, response))
-            myOhs = myOhs + (kv._1 -> response.authenticatedRh)
-            if (currentSuccessSet.size == 5) { //5 is quorumSize
-              cancelable.cancel()
-              promise success ((successSet.values.toSet, myOhs))
+            //mutex needed because of multithreaded ex context
+            this.synchronized {
+              myOhs = myOhs + (kv._1 -> response.authenticatedRh)
+              currentSuccessSet = currentSuccessSet + ((kv._1, response))
+              if (currentSuccessSet.size == q) {
+                cancelable.cancel()
+                completionPromise success ((successSet.values.toSet, myOhs))
+              }
             }
-          case _ => //this can be removed by pecifying other...
+          case _ => //no need to do nothing, only waiting for other servers' responses
         }))
+
+      completionPromise.future
     }
 
+    def getElected(responses: Set[Response[Option[T]]]) = Future {
+      responses
+        .groupMapReduce(identity)(_ => 1)(_ + _)
+        .maxByOption(_._2)
+        .getOrElse(throw new RuntimeException("inconsistent protocl state"))
+    }
 
-    val promise = Promise[(Set[Response[T, U]], OHS[U])]()
+    //actual logic
     for {
-      (successSet, ohs) <- promise.future
-      //extract response
-    } yield (2, 2, ohs)
-    null
+      (responses, ohs) <- quorumImpl2(Promise(), successSet = Map())
+      (response, voteCount) <- getElected(responses)
+    } yield (response.answer, voteCount, ohs)
   }
 
-  //to move in scheduler class
-  private def scheduleOnceWithPromise(duration: FiniteDuration): Future[Void] = {
-    val promise = Promise()
-    scheduler.scheduleOnce (3.seconds) (())
-    promise.future
-  }
-
-  //doing nothing
-  private def backOff() = scheduleOnceWithPromise(3.seconds)
 }
