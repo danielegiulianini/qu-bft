adb5174727f9a0d0ebc6ad896f0fb45987ce93eb
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-05-12 16:08:40 +0200

    add Storage with underlying mutable and synchronized map

--- qu-common-presentation/src/main/scala/SeparateStorage.scala
+++ qu-common-presentation/src/main/scala/SeparateStorage.scala
@@ -1,8 +1,8 @@
-import qu.protocol.model.ConcreteQuModel
-import qu.protocol.model.ConcreteQuModel.LogicalTimestamp
+import qu.protocol.model.ConcreteQuModel._
 
 import scala.reflect.runtime.universe._
 import java.util.Objects
+import java.util.concurrent.ConcurrentHashMap
 
 //storage separate from QUModel (this class can be generic in U(ObjecT)
 class SeparateStorage[U: TypeTag] {
@@ -21,8 +21,8 @@ class SeparateStorage[U: TypeTag] {
   }
 
 
-  def retrieve[T: TypeTag, A: TypeTag](logicalTimestamp: LogicalTimestamp): Option[(U, Option[T])] = {
-    //really needed?
+  def retrieve[T: TypeTag](logicalTimestamp: LogicalTimestamp): Option[(U, Option[T])] = {
+    //todo really needed?
     val tmp = synchronized {
       storage.get(implicitly[TypeTag[T]])
     }
@@ -38,10 +38,43 @@ class SeparateStorage[U: TypeTag] {
 
 }
 
+
+import collection.mutable.{Map => MutableMap}
+import collection.mutable.{HashMap => MutableHashMap}
+
+
+abstract class MutableStorage[U: TypeTag] {
+  protected val storage: MutableMap[TypeTag[_], MutableMap[LogicalTimestamp, (U, Option[Any])]]
+
+  def store[T: TypeTag](logicalTimestamp: LogicalTimestamp, objectAndAnswer: (U, Option[T])): Unit =
+    storage.getOrElseUpdate(Objects.requireNonNull(implicitly[TypeTag[T]]), new MutableHashMap()).put(logicalTimestamp, objectAndAnswer) //Map[LogicalTimestamp, (_, Option[_])]()
+
+  def retrieve[T: TypeTag](logicalTimestamp: LogicalTimestamp): Option[(U, Option[T])] =
+    storage.get(implicitly[TypeTag[T]]).flatMap(_.get(logicalTimestamp).asInstanceOf[Option[(U, Option[T])]])
+}
+
+class ThreadSafeMutableStorage[U:TypeTag] extends MutableStorage[U] {
+  override val storage = new scala.collection.concurrent.TrieMap[TypeTag[_], MutableMap[LogicalTimestamp, (U, Option[Any])]]()
+}
+
+
+
+
 object UseCase extends App {
   val storage = new SeparateStorage[Int]()
-  val myLt = ConcreteQuModel.MyLogicalTimestamp(2, false, Some("id1"), Option.empty, Option.empty)
-  storage.store(myLt, (2, Some("io"))) //type param is fundamental (if don't passed the typetag is nothing and nothig (literaly) is returned
-  val retrieved = storage.retrieve[String, String](myLt) //type param is fundamental (if don't passed the typetag is nothing and nothig (literaly) is returned
+  val myLt = MyLogicalTimestamp(2, false, Some("id1"), Option.empty, Option.empty)
+  storage.store[String](myLt, (2, Some("io"))) //type param is fundamental (if don't passed the typetag is nothing and nothig (literaly) is returned
+  val retrieved = storage.retrieve[String](myLt) //type param is fundamental (if don't passed the typetag is nothing and nothig (literaly) is returned
   println("retrieved is: " + retrieved)
+
+
+  println("now testing mutableStorage")
+  val mutableStorage = new ThreadSafeMutableStorage[Int]()
+  val myLt2 = MyLogicalTimestamp(2, false, Some("id1"), Option.empty, Option.empty)
+  storage.store[String](myLt, (2, Some("io"))) //type param is fundamental (if don't passed the typetag is nothing and nothig (literaly) is returned
+  val retrieved2 = storage.retrieve[String](myLt) //type param is fundamental (if don't passed the typetag is nothing and nothig (literaly) is returned
+  println("retrieved is: " + retrieved2)
+
 }
+
+
--- qu-common-presentation/src/main/scala/qu/protocol/model/Storage.scala
+++ qu-common-presentation/src/main/scala/qu/protocol/model/Storage.scala
@@ -1,33 +1,38 @@
 package qu.protocol.model
 
+import java.util.Objects
+import scala.reflect.runtime.universe._
 
-//could be a class separated from QUModel, bound (for LogicalTimestamp) to a ConcreteImplementation
+
+//could be a class separated from QUModel, bound (for LogicalTimestamp) to a ConcreteImplementation of QuModel
 trait Storage {
   self: AbstractQuModel =>
 
-  def store[T, U](logicalTimestamp: LogicalTimestamp, objectAndAnswer: (U, Option[T])): Unit
+  def store[T:TypeTag, U:TypeTag](logicalTimestamp: LogicalTimestamp, objectAndAnswer: (U, Option[T])): Unit
 
-  def retrieve[T, U](logicalTimestamp: LogicalTimestamp): Option[(U, Option[T])]
+  def retrieve[T:TypeTag, U:TypeTag](logicalTimestamp: LogicalTimestamp): Option[(U, Option[T])]
 
 }
 
 trait InMemoryStorage extends Storage {
   self: AbstractQuModel =>
 
-  override def store[T, U](logicalTimestamp: LogicalTimestamp, objectAndAnswer: (U, Option[T])) = ???
+  private var storage: Map[(TypeTag[_], TypeTag[_]), (LogicalTimestamp, Option[Any], Any)] = Map()
 
-  override def retrieve[T, U](logicalTimestamp: LogicalTimestamp): Option[(U, Option[T])] = ???
+  //favorites = favorites + (Objects.requireNonNull(implicitly[TypeTag[T]]) -> instance)
+  override def store[T:TypeTag, U:TypeTag](logicalTimestamp: LogicalTimestamp, objectAndAnswer: (U, Option[T])) = ???
+    //storage + ( Objects.requireNonNull(implicitly[TypeTag[T]]) -> Objects.requireNonNull(implicitly[TypeTag[U]] ->)
 
+  override def retrieve[T:TypeTag, U:TypeTag](logicalTimestamp: LogicalTimestamp): Option[(U, Option[T])] =
+    ???
 }
 
+/*
 trait PersistentStorage extends Storage {
   self: AbstractQuModel =>
-
 }
-
 trait PersistentCachingStorage extends Storage {
   self: AbstractQuModel =>
-
 }
-
+*/
 
