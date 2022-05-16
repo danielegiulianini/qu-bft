10e88be1bbc5f2b299f49022c06fae89d4c35117
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-05-16 09:38:39 +0200

    add another style (less verbose) for building AuthenticatedQuClients

--- qu-client/src/main/scala/QuClient.scala
+++ qu-client/src/main/scala/QuClient.scala
@@ -1,7 +1,8 @@
-import GrpcClientStub.JacksonClientStub
+import GrpcClientStub.UnauthenticatedJacksonClientStub
+import QuorumPolicy.{PolicyFactory, simpleJacksonPolicyFactoryUnencrypted}
 import com.fasterxml.jackson.module.scala.JavaTypeable
 import io.grpc.ManagedChannelBuilder
-import qu.protocol.Shared.{QuorumSystemThresholds, ServerInfo}
+import Shared.{QuorumSystemThresholds, RecipientInfo => ServerInfo}
 
 import scala.concurrent.Future
 
@@ -9,53 +10,67 @@ import scala.concurrent.Future
 import qu.protocol.model.ConcreteQuModel._
 
 //most abstract possible (not bound to grpc)
-trait QuClient[U, Marshallable[_]] {
+trait QuClient[U, Transferable[_]] {
   def submit[T](op: Operation[T, U])(implicit
-                                     marshallableRequest: Marshallable[Request[T, U]],
-                                     marshallableResponse: Marshallable[Response[Option[T]]],
-                                     marshallableRequestObj: Marshallable[Request[Object, U]],
-                                     marshallableResponseObj: Marshallable[Response[Option[Object]]]): Future[T]
+                                     marshallableRequest: Transferable[Request[T, U]],
+                                     marshallableResponse: Transferable[Response[Option[T]]],
+                                     marshallableRequestObj: Transferable[Request[Object, U]],
+                                     marshallableResponseObj: Transferable[Response[Option[Object]]]): Future[T]
 }
 
 object QuClient {
-  //todo should be put in the co of the class of the object that it creates (QuClientImpl) if wanting to forbid
-  // creating QuClientImpl without this builder...
-  trait AuthenticatedClientBuilderD[U]
-
-  case class AuthenticatedClientBuilder[U, Marshallable[_]](private val serversInfo: Set[ServerInfo],
-                                                            private val thresholds: Option[QuorumSystemThresholds],
-                                                            private val policyFactory: (Set[ServerInfo], QuorumSystemThresholds) => QuorumPolicy[U, Marshallable]
-                                                           ) extends AuthenticatedClientBuilderD[U] {
-    //i have to pass to quClient: policy (server stubs)
-    def addServer(serverInfo: ServerInfo): AuthenticatedClientBuilder[U, Marshallable] = {
-      this.copy(serversInfo = serversInfo + serverInfo)
-      //could create channels here instead
-    }
-
-    def withThresholds(thresholds: QuorumSystemThresholds): AuthenticatedClientBuilder[U, Marshallable] = {
-      this.copy(thresholds = Some(thresholds))
-    }
-
-    def build = new QuClientImpl[U, Marshallable](policyFactory(serversInfo, thresholds.get), thresholds.get)
-  }
 
   //should return a trait type
-  def defaultBuilder[U](): AuthenticatedClientBuilderD[U] = simpleJacksonQuorumPolicyBuilder[U]()
+  //could use builder factory instead of defaultBuilder
+  def defaultBuilder[U](token: String): AuthenticatedClientBuilderD[U] = simpleJacksonQuClientBuilderInFunctionalStyle[U](token)
 
-  //specific builder instance-related utility
-  private object AuthenticatedClientBuilder {
-    def empty[U, Marshallable[_]](policyFactory: (Set[ServerInfo],
-      QuorumSystemThresholds) => QuorumPolicy[U, Marshallable]) =
-      AuthenticatedClientBuilder(Set(), Option.empty, (mySet, thresholds) => policyFactory(mySet, thresholds))
+  //todo complete methods (abstraction to hide Marshallable type param) but I think it is not possible...
+  trait AuthenticatedClientBuilderD[U] {
+    //def build() : QuClient[U, ]
   }
 
-  //implementations
-  private def simpleJacksonQuorumPolicyBuilder[U](): AuthenticatedClientBuilderD[U] =
-    AuthenticatedClientBuilder.empty[U, JavaTypeable]((mySet, thresholds) =>
-      new SimpleBroadcastPolicy(thresholds,
-        mySet.map(serverInfo => serverInfo.ip -> new JacksonClientStub(
-          ManagedChannelBuilder.forAddress(serverInfo.ip,
-            serverInfo.port)
-            .build))
-          .toMap))
+  //another style of building objects
+  case class AuthenticatedClientBuilderWithNamedParam[U, Transportable[_]]( //programmer dependencies
+                                                                            private val policyFactory: (Set[ServerInfo], QuorumSystemThresholds) => QuorumPolicy[U, Transportable],
+                                                                            //user dependencies
+                                                                            private val serversInfo: Set[ServerInfo],
+                                                                            private val thresholds: QuorumSystemThresholds,
+                                                                            private val token: String
+                                                                          ) extends AuthenticatedClientBuilderD[U] {
+    def build: QuClientImpl[U, Transportable] = {
+      //todo missing validation
+      new QuClientImpl[U, Transportable](policyFactory(serversInfo, thresholds), thresholds)
+    }
+  }
+
+  case class AuthenticatedClientBuilderInFunctionalStyle[U, Transportable[_]]( //programmer dependencies
+                                                                               private val policyFactory: (Set[ServerInfo], QuorumSystemThresholds) => QuorumPolicy[U, Transportable],
+                                                                               //user dependencies
+                                                                               private val serversInfo: Set[ServerInfo],
+                                                                               private val thresholds: Option[QuorumSystemThresholds],
+                                                                               private val token: String
+                                                                             ) extends AuthenticatedClientBuilderD[U] {
+
+    def addServer(serverInfo: ServerInfo): AuthenticatedClientBuilderInFunctionalStyle[U, Transportable] =
+      this.copy(serversInfo = serversInfo + serverInfo) //could create channels here instead of creating in policy
+
+    def withThresholds(thresholds: QuorumSystemThresholds): AuthenticatedClientBuilderInFunctionalStyle[U, Transportable] =
+      this.copy(thresholds = Some(thresholds))
+
+    def build: QuClientImpl[U, Transportable] = {
+      //todo missing validation
+      new QuClientImpl[U, Transportable](policyFactory(serversInfo, thresholds.get), thresholds.get)
+    }
+  }
+
+  //builder companion object specific builder instance-related utility
+  private object AuthenticatedClientBuilderInFunctionalStyle {
+    def empty[U, Transferable[_]](policyFactory: PolicyFactory[Transferable, U], token: String): AuthenticatedClientBuilderInFunctionalStyle[U, Transferable] =
+      AuthenticatedClientBuilderInFunctionalStyle((mySet, thresholds) => policyFactory(mySet, thresholds), Set(), Option.empty, token)
+  }
+
+  //hided builder implementations
+  private def simpleJacksonQuClientBuilderInFunctionalStyle[U](token: String): AuthenticatedClientBuilderD[U] =
+    AuthenticatedClientBuilderInFunctionalStyle.empty[U, JavaTypeable](simpleJacksonPolicyFactoryUnencrypted(token), token)
+
 }
