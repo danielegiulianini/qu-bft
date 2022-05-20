d04cbd4d78e1c1a0f80ed864d771cffb964a2f49
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-05-20 16:59:19 +0200

    convert custom exception to grpc status for a richer and more informative error model at client side

--- auth/src/main/scala/qu/auth/LocalAuthenticator.scala
+++ auth/src/main/scala/qu/auth/LocalAuthenticator.scala
@@ -13,20 +13,21 @@ class LocalAuthenticator {
     this.synchronized {
       if (usersByUsername.contains(user.username)) throw new ConflictException("Username already exists: " + user.username)
       val toBeAdded = user.copy() // defensive copy
-      usersByUsername + (user.username, toBeAdded)
+      usersByUsername + (user.username -> toBeAdded)
     }
   }
 
   @throws[BadContentException]
   @throws[WrongCredentialsException]
-  def authorize(credentials: Credentials): Nothing = {
+  def authorize(credentials: Credentials): Token = {
     //I assume credentials can't be null, so I omit: if (user == null ||...
     if (credentials.username.isBlank) throw new BadContentException("Missing user ID: " + credentials.username)
     if (credentials.password.isBlank) throw new BadContentException("Missing password: " + credentials.password)
     val userId = credentials.username
     this.synchronized {
       val user = usersByUsername.get(userId).orElse(throw new WrongCredentialsException("No such a user: " + userId))
-      if (!credentials.password.equals(user.get)) throw new WrongCredentialsException("Wrong credentials for user: " + userId)
+      if (!credentials.password.equals(user.get.password)) throw new WrongCredentialsException("Wrong credentials for user: " + userId)
+      //assigning "client" role to all clients
       new Token(user.get.username, Role.CLIENT)
     }
   }
--- auth/src/main/scala/qu/auth/MyAuthService.scala
+++ auth/src/main/scala/qu/auth/MyAuthService.scala
@@ -1,12 +1,56 @@
 package qu.auth
+
+import io.grpc.Status
+
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.util.{Failure, Success}
 
 class MyAuthService extends AuthGrpc.Auth {
 
-  private val usersByUsername = new Nothing
-  private val usersByEmail = new Nothing
+  private val localAuthenticator: LocalAuthenticator = new LocalAuthenticator
 
-  override def register(request: Credentials): Future[RegisterResponse] = ???
+  //mapping custom exception to transport layer code for:
+  //1. separation of concerns (not cluttering app logic code with transport layer error codes): so separating a reusable localAuthenticator
+  //2. StatusRuntimeException swallows the error message and assigns a default status code UNKNOWN at client side.Failure(io.grpc.StatusRuntimeException: UNKNOWN)
+  override def register(request: User): Future[RegisterResponse] = {
+    //se c'è problema nel local authenticator devo settare l'onError dell'observer
+    //todo or delegate to external pool?
+    mapThrowableByStatus(Future.successful(
+      localAuthenticator.register(request)
+    ).map(_ => RegisterResponse()), {
+      case _: BadContentException => Status.INVALID_ARGUMENT
+      case _: ConflictException => Status.ALREADY_EXISTS
+    })
+  }
 
-  override def authorize(request: Credentials): Future[Token] = ???
+  override def authorize(request: Credentials): Future[Token] = {
+    mapThrowableByStatus(Future.successful(
+      localAuthenticator.authorize(request)
+    ), {
+      case _: BadContentException => Status.INVALID_ARGUMENT
+      case _: WrongCredentialsException => Status.NOT_FOUND
+    })
+  }
+
+  private def mapThrowableByStatus[T](f: Future[T], f2: Throwable => Status) : Future[T] = f transform {
+    mapThrowable(f, error => f2(error).withDescription(error.getMessage()).withCause(error)
+      .asException())
+  }
+
+  private def mapThrowable[T](f: Future[T], exFact: Throwable => Throwable) = f transform {
+    case s@Success(_) => s
+    case Failure(cause) => Failure(exFact(cause))
+  }
+
+
+  //TODO reusable, general utilities: could be plugged with implicit conversion on Future type
+  private def mapException[T](f: Future[T], ex: Exception) = f transform {
+    case s@Success(_) => s
+    case Failure(cause) => {
+      Failure(ex)
+    }
+  }
+
+
 }
