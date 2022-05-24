e032fb568fb650d6c81ccb4c6306d1e981711675
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-05-24 12:19:49 +0200

    add skeleton for QuService instantiation

--- qu-system-testing/src/main/scala/qu/ExampleTesting.scala
+++ qu-system-testing/src/main/scala/qu/ExampleTesting.scala
@@ -1,3 +1,85 @@
-package qu object ExampleTesting {
+package qu
 
+import com.fasterxml.jackson.module.scala.JavaTypeable
+
+object ExampleTesting extends App {
+  //val a: Server = ServerBuilder.forPort(1).build()
+  //ManagedChannelBuilder
+  //Grpc.newChannelBuilder(
+  /*
+    class Fetch extends Operation[String, Int] {
+      override def compute(obj: Int): String = "hello"
+    }
+
+    val myService = new QuService //builder mio
+    myService.addOperation[String]()
+
+    val server = InProcessServerBuilder
+    //here you specify the executor
+      .forName("cc")
+      .addService(myService)
+      .build
+    server.start
+    Thread.sleep(1000)
+
+    val chan = InProcessChannelBuilder.forName("cc").build //ManagedChannelBuilder
+    val stb = new JacksonClientStub[Int](chan)
+
+    val myFuture = stb.send(Request(new Fetch, null))
+    myFuture.onComplete({
+      case Success(_) => println("future successed!")
+      case Failure(exception) =>
+        println(s"futures failed!, ${exception.getMessage}")
+    })
+
+    server.shutdown()
+
+    import java.util.concurrent.TimeUnit
+
+    server.awaitTermination(5, TimeUnit.SECONDS)*/
+
+  def ola[T: JavaTypeable](a: T) = {
+    println("ciao")
+  }
+
+  class Bello[Y] {
+    def compute(x: Y) = x
+  }
+
+  ola(new Bello)
+
+  class Wrapping[T](a: T)
+
+  def metodoCheRichiedeImplicitDelWrappante[T](a: T)(implicit ab: JavaTypeable[Wrapping[T]]): Unit = {
+  }
+
+  //metodo che riduce impliciti
+  def metodoCheRichiedeImplicitiSoloDelWrappato[T: JavaTypeable](a: T): Unit = {
+    metodoCheRichiedeImplicitDelWrappante(new Wrapping(a))
+  } // import com.roundeights.hasher.Digest.digest2string
+
+
+  // println("l'hmac e':"+ "".hmac("ciao").md5)
+  val t = Set(1, 2, 3)
+  val myMap = t.map(id => id -> "ciao").toMap
+  val myMap2 = t.map(_ -> "ciao").toMap
+
+  println(myMap)
+
+  //closure analysis in scala
+  class StubClient {
+    def ciao(a: () => Unit) = a()
+  }
+
+  val t2 = new StubClient()
+  t2.ciao(() => println(t2))
+
+  class MyClass {
+    var a: Int = _
+    var stubCl: StubClient = _
+
+    def myMeth() = stubCl
+  }
+
+  println("cosa contiene l'bj di MyClass?: " + new MyClass().myMeth())
 }
--- qu-system-testing/src/main/scala/qu/ExampleTesting2.scala
+++ qu-system-testing/src/main/scala/qu/ExampleTesting2.scala
@@ -1,3 +1,58 @@
-package qu object ExampleTesting2 {
+package qu
 
+import io.grpc.inprocess.InProcessServerBuilder
+import qu.StubFactories.inNamedProcessJacksonStubFactory
+import qu.model.ConcreteQuModel.Request
+import qu.model.ConcreteQuModel.{Request, Response, Query}
+
+
+import java.util.concurrent.TimeUnit
+
+object ExampleTesting2 extends App {
+  println("try to send to server with a jacksonclientStub...")
+/*
+  val serviceFactory = jacksonSimpleQuorumServiceFactory[Int]()
+
+  val ip = "ciao"
+  val port = 2
+  val serviceInfo = RecipientInfo(ip = ip, port = port, keySharedWithMe = "hmackey")
+
+
+  //quorumSystemThresholds, serverInfo, obj
+  val service = serviceFactory(null, serviceInfo, 8)
+
+
+  class MyQuery extends Query[Int, Int] {
+    override def whatToReturn(obj: Int): Int = obj
+  }
+
+  service.addOp[Int]()
+
+
+  //simulare una InprocessQuServer
+  val server = InProcessServerBuilder
+    .forName(ip + ":" + port) //.intercept(new AuthorizationServerInterceptor())
+    .addService(service)
+    .build
+
+  //must register operations!
+
+  server.start()
+
+  Thread.sleep(1000)
+
+
+  //jacksonclientstub con canale inprocess
+  val myStub = inNamedProcessJacksonStubFactory(serviceInfo)
+
+  myStub.send2(
+    Request(operation = Option.empty[Query[Int, String]],
+      ohs = null))
+
+
+  Thread.sleep(4000)
+  println("closing...")
+
+  server.shutdown()
+  server.awaitTermination(5, TimeUnit.SECONDS)*/
 }
