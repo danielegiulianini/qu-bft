769f8f75c0c0210746c19175b13944443edf5f65
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-05-24 12:13:55 +0200

    adapt client and server to new description of server (ip, port, key not in ServiceInfo)

---
+++ qu-client/src/main/scala/qu/client/AuthenticatedClientBuilderInFunctionalStyle.scala
@@ -0,0 +1,37 @@
+package qu.client
+
+import com.fasterxml.jackson.module.scala.JavaTypeable
+import qu.client.ClientQuorumPolicy.{ClientPolicyFactory, simpleJacksonPolicyFactoryUnencrypted}
+import qu.model.QuorumSystemThresholds
+
+
+case class AuthenticatedClientBuilderInFunctionalStyle[U, Transportable[_]]( //programmer dependencies
+                                                                             private val policyFactory: (Map[String, Int], QuorumSystemThresholds) => ClientQuorumPolicy[U, Transportable],
+                                                                             //user dependencies
+                                                                             private val serversInfo: Map[String, Int],
+                                                                             private val thresholds: Option[QuorumSystemThresholds],
+                                                                           ) {
+  def addServer(ip: String, port: Int): AuthenticatedClientBuilderInFunctionalStyle[U, Transportable] =
+    this.copy(serversInfo = serversInfo + (ip -> port)) //could create channels here instead of creating in policy
+
+  def withThresholds(thresholds: QuorumSystemThresholds): AuthenticatedClientBuilderInFunctionalStyle[U, Transportable] =
+    this.copy(thresholds = Some(thresholds))
+
+  def build: AuthenticatedQuClientImpl[U, Transportable] = {
+    //todo missing validation
+    new AuthenticatedQuClientImpl[U, Transportable](policyFactory(serversInfo, thresholds.get), serversInfo.keySet, thresholds.get)
+  }
+}
+
+//builder companion object specific builder instance-related utility
+object AuthenticatedClientBuilderInFunctionalStyle {
+
+  def builder[U](token: String): AuthenticatedClientBuilderInFunctionalStyle[U, JavaTypeable] = simpleJacksonQuClientBuilderInFunctionalStyle[U](token)
+
+  private def empty[U, Transferable[_]](policyFactory: ClientPolicyFactory[Transferable, U], token: String): AuthenticatedClientBuilderInFunctionalStyle[U, Transferable] =
+    AuthenticatedClientBuilderInFunctionalStyle((mySet, thresholds) => policyFactory(mySet, thresholds), Map(), Option.empty)
+
+  //builder implementations
+  def simpleJacksonQuClientBuilderInFunctionalStyle[U](token: String): AuthenticatedClientBuilderInFunctionalStyle[U, JavaTypeable] =
+    AuthenticatedClientBuilderInFunctionalStyle.empty[U, JavaTypeable](simpleJacksonPolicyFactoryUnencrypted(token), token)
+}
---
+++ qu-client/src/main/scala/qu/client/AuthenticatingClient.scala
@@ -0,0 +1,39 @@
+package qu.client
+
+import com.fasterxml.jackson.module.scala.JavaTypeable
+import qu.auth.AuthClient
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import qu.client.AuthenticatedClientBuilderInFunctionalStyle.simpleJacksonQuClientBuilderInFunctionalStyle
+import qu.model.QuorumSystemThresholds
+
+//client for providing username (or registering)
+//now modeled as normal class, then could think of a builder
+case class AuthenticatingClient[U](ip:String, port:Int,
+                                   username: String,
+                                   password: String) {
+  //creates the channel and passes it to stubs
+
+  //APIs: authorize, register, remove, edit, get, getall
+ // AuthClient()
+
+  def register(): Future[QuClient[U, JavaTypeable]] = {
+    //call to auth service...
+    //for { registered <- register }
+    null
+  }
+
+  def authorize():
+  Future[AuthenticatedClientBuilderInFunctionalStyle[U, JavaTypeable]] = {
+    //se mi da il token correttamente allora ok altrimenti mi da future failed (dovrebbe già
+    //tutto essere incapsulato nella chiamata)
+    val token = ""
+
+    //call to auth service...
+    //it's to be inserted in for comprehension
+    Future {
+      simpleJacksonQuClientBuilderInFunctionalStyle[U](token = token)
+    }
+  }
+}
