b6bdd1cd521dccb89cb267e8d1d24c1d29597f15
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-06-06 15:26:59 +0200

    avoid using java hashCode for hashing operations

--- qu-core/src/main/scala/qu/model/Crypto.scala
+++ qu-core/src/main/scala/qu/model/Crypto.scala
@@ -11,11 +11,11 @@ trait CryptoMd5Authenticator {
   override def nullAuthenticator: α = Map[String, String]()
 
   //adapted from https://gist.github.com/ohac/310945/7642d5432ca5f38d6341d7e7076073d98354c1a7, leveraging sortedSet ordering here
-  def hmac(key: String, replicaHistory: ReplicaHistory): HMAC = {
+  def hmac(key: Key, replicaHistory: ReplicaHistory): HMAC = {
     hmacString(key, replicaHistory.hashCode().toString)
   }
 
-  def hmacString(key:String, data:String): String ={
+  def hmacString(key: String, data: String): String = {
     import javax.crypto.Mac
     import javax.crypto.spec.SecretKeySpec
     val sha256_HMAC = Mac.getInstance("HmacSHA256")
@@ -25,11 +25,14 @@ trait CryptoMd5Authenticator {
     sha256_HMAC.doFinal(data.getBytes("UTF-8")).map(_.toString).mkString(",")
   }
 
-
-  def updateAuthenticatorFor(keys: Map[ServerId, String])(serverIdToUpdate: ServerId)(replicaHistory: ReplicaHistory): α
+  def authenticateRh(rh: ReplicaHistory, keys: Map[ServerId, Key]): α =
+    keys.view.mapValues(hmac(_, rh)).toMap
+/*
+  def updateAuthenticatorFor(keys: Map[ServerId, Key])(serverIdToUpdate: ServerId)(replicaHistory: ReplicaHistory): α
   = if (replicaHistory == emptyRh) nullAuthenticator //could be removed as updatedReplicaHistory will not ever hadve
-  else fillAuthenticatorFor(keys)(serverIdToUpdate)(hmac(_, replicaHistory))
-
+  else keys.map{case (serverId, key) => if (serverId == serverIdToUpdate) serverId -> authenticateRh(replicaHistory, key) else }// fillAuthenticatorFor(keys)(serverIdToUpdate)(hmac(_, replicaHistory))
+  //if serverId == scelto then mappa con la authenticatedRh altrimenti lascia come è
+*/
 }
 
 
--- qu-core/src/main/scala/qu/model/Hashing.scala
+++ qu-core/src/main/scala/qu/model/Hashing.scala
@@ -11,7 +11,14 @@ trait Hashing {
   override def represent(ohs: OHS): OHSRepresentation =
     hashObject(ohs)
 
-  //since obj is never null Objects.hashcode not required...
-  private def hashObject(obj: Any) = obj.hashCode().toString //obj.toString().md5.hex
+  private def hashObject(obj: Any): String =
+    //obj.hashCode().toString //obj.toString().md5.hex   //since obj is never null Objects.hashcode not required...(not to use in distributed applications
+  {
+    import java.nio.charset.StandardCharsets
+    import java.security.MessageDigest
+    val digest = MessageDigest.getInstance("SHA-256")
+    val hash = digest.digest(obj.toString.getBytes(StandardCharsets.UTF_8))
+    hash.map(_.toString).mkString(",")
+  }
 
 }
