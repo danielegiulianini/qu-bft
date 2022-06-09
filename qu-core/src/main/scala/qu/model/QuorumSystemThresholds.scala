eab1e56c8c36f4e10e3cea98cee70eab777008ef
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-06-09 10:07:43 +0200

    refactor utilities in Thresholds

--- qu-core/src/main/scala/qu/model/QuorumSystemThresholds.scala
+++ qu-core/src/main/scala/qu/model/QuorumSystemThresholds.scala
@@ -2,17 +2,20 @@ package qu.model
 
 case class QuorumSystemThresholds(t: Int, q: Int, b: Int, r: Int, n: Int) {
   //validation
+
 }
 
 object QuorumSystemThresholds {
-  //factory for only 3 param (the other with default)
-  def apply(t: Int, b: Int): QuorumSystemThresholds = QuorumSystemThresholds(t, 2 * t + 2 * b + 1, b, t + b + 1, getN(t = t, b = b))
+  //factory for only 2 param (the other with default)
+  def apply(t: Int, b: Int): QuorumSystemThresholds =
+    QuorumSystemThresholds(t, getQFromTAndB(t, b), b, getRFromTAndB(t, b), getNFromTAndB(t = t, b = b))
 
-  def apply(t: Int, q: Int, b: Int): QuorumSystemThresholds = QuorumSystemThresholds(t, q, b, t + b + 1, getN(t = t, b = b))
+  //factory for only 2 param (the other with default)
+  def apply(t: Int, q: Int, b: Int): QuorumSystemThresholds =
+    QuorumSystemThresholds(t, q, b, t + b + 1, getNFromTAndB(t = t, b = b))
 
-  private def getN(t: Int, b: Int): Int = 3 * t + 2 * b + 1
-}
+  private def getNFromTAndB(t: Int, b: Int): Int = 3 * t + 2 * b + 1
+  private def getQFromTAndB(t: Int, b: Int): Int = 2 * t + 2 * b + 1
+  private def getRFromTAndB(t: Int, b: Int): Int = t + b + 1
 
-object Prova {
-  val j: QuorumSystemThresholds = QuorumSystemThresholds(1, 2, 3)
 }
